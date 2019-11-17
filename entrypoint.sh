25db8087e2d02fce54e979aa78ba3afc641f6871
Khaazz <khaaz.dev@gmail.com>
2019-11-17 15:04:46 +0100

    Forgot the @ for the scope

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ cd $GITHUB_WORKSPACE
 
 SCOPE=`sed -e "s/\/.*//g" <<< $GITHUB_REPOSITORY`
 
-cat package.json | sed -i -e 's/"name": "\([a-z0-9]*\)"/"name": "'$SCOPE'\/\1"/' package.json
+cat package.json | sed -i -e 's/"name": "\([a-z0-9]*\)"/"name": "@'$SCOPE'\/\1"/' package.json
