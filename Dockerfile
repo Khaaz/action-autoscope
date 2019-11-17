5125cd4c451538b86af9e17c412027506d0069ec
Khaazz <khaaz.dev@gmail.com>
2019-11-17 15:39:54 +0100

    Check for env variable before auto filling owner

--- Dockerfile
+++ Dockerfile
@@ -4,7 +4,5 @@ FROM alpine:latest
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
 
-ENV SCOPE_ENV $SCOPE
-
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,14 @@
 
 set -eo pipefail
 
-echo $SCOPE_ENV
+cd $GITHUB_WORKSPACE
 
 echo "Identify Github owner (scope)"
-SCOPE=`sed -e "s/\/.*//g" <<< $GITHUB_REPOSITORY`
+if [ -n "$SCOPE" ] && ! [ -z "$SCOPE" ]; then
+    OWNER=$SCOPE
+else
+    OWNER=`sed -e "s/\/.*//g" <<< $GITHUB_REPOSITORY`
+fi
 
 echo "Scope the package in package.json"
-cat package.json | sed -i -e 's/"name": "\([a-z0-9]*\)"/"name": "@'$SCOPE'\/\1"/' package.json
+cat package.json | sed -i -e 's/"name": "\([a-z0-9]*\)"/"name": "@'$OWNER'\/\1"/' package.json
