9ed91c79148f5d4625a6c4a4745861f2289427ff
Khaazz <khaaz.dev@gmail.com>
2019-11-17 15:20:02 +0100

    Adds env variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,10 @@
 
 set -eo pipefail
 
-cd $GITHUB_WORKSPACE
+echo $SCOPE_ENV
 
+echo "Identify Github owner (scope)"
 SCOPE=`sed -e "s/\/.*//g" <<< $GITHUB_REPOSITORY`
 
+echo "Scope the package in package.json"
 cat package.json | sed -i -e 's/"name": "\([a-z0-9]*\)"/"name": "@'$SCOPE'\/\1"/' package.json
